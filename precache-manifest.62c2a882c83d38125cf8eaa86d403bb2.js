8e6a3eb64bfbf36c3e55ff1444ffd3b753b132c0
ayushisingla <ayushisingla46@gmail.com>
2020-06-15 12:45:30 +0530

    Updates

---
+++ precache-manifest.62c2a882c83d38125cf8eaa86d403bb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7de90eeda1cce408ade07f13a231498c",
+    "url": "/progressApp/index.html"
+  },
+  {
+    "revision": "47ad90df3bf7f9f0c445",
+    "url": "/progressApp/static/css/2.1b1c968b.chunk.css"
+  },
+  {
+    "revision": "67f2ac19211e36db821a",
+    "url": "/progressApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "47ad90df3bf7f9f0c445",
+    "url": "/progressApp/static/js/2.3b3b3c5d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/progressApp/static/js/2.3b3b3c5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67f2ac19211e36db821a",
+    "url": "/progressApp/static/js/main.b7f8df57.chunk.js"
+  },
+  {
+    "revision": "5a1079a31169b6d6463c",
+    "url": "/progressApp/static/js/runtime-main.81c6c3c5.js"
+  }
+]);
